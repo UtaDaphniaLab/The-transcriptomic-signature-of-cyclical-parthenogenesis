45f8322c920976a8af72e1dbd844a4c6e9339105
TrungHuynh4 <trung.huynh@mavs.uta.edu>
2022-11-09 12:52:03 -0600

    Update Differential Expression of Pooled Samples.R

--- Differential-expression-analysis/Differential Expression of Pooled Samples.R
+++ Differential-expression-analysis/Differential Expression of Pooled Samples.R
@@ -3,11 +3,11 @@ library("readr")
 library("jsonlite")
 library("DESeq2")
 
-compare1 = "LM"
-compare2 = "LA"
-output_dir = "E:/School Stuff/Lab Documents/RNA_Seq_Analysis/Refined Analysis/Refined_Analysis/Clone_Stage_Model/EMvEA"
+# Specify which stages to compare (EM/EA/LM/LA)
+compare1 = "EM"
+compare2 = "EA"
 
-dir <- "E:/School Stuff/Lab Documents/RNA_Seq_Analysis/Refined Analysis/Salmon_Quant_Full_Real_Name"
+dir <- choose.dir(caption = "Choose sample directory")
 sample_names <- list.files(dir, pattern = paste("-", compare1, "-|-", compare2, "-", sep = ""))
 
 
@@ -21,7 +21,7 @@ files <- file.path(dir, samples_daphnia$Sample_Dir, "quant.sf")
 
 names(files) <- samples_daphnia$Sample_Dir
 
-tx2gene <- read_csv("E:/School Stuff/Lab Documents/RNA_Seq_Analysis/Refined Analysis/Refined_Analysis/tx_file.cvs")
+tx2gene <- read_csv(choose.files(caption = "Choose tx file"))
 txi <- tximport(files, type="salmon", tx2gene=tx2gene)
 
 ddsTxi <- DESeqDataSetFromTximport(txi,
@@ -31,23 +31,6 @@ ddsTxi <- DESeqDataSetFromTximport(txi,
 dds <- DESeq(ddsTxi)
 res <- results(dds, contrast=c("Stage", compare1, compare2))
 
-write.table(res,
-            file.path(output_dir,paste(compare1,"v",compare2,"_","clone_stage_all.txt", sep=""))
-            ,sep="\t", quote=FALSE)
-
 write.table(res[ which(res$padj < 0.05 & abs(res$log2FoldChange) > 0.5849625007211562),],
-            file.path(output_dir,paste(compare1,"v",compare2,"_","clone_stage","_0.05padj_1.5FC.txt", sep=""))
+            file.path(output_dir,paste(compare1,"v",compare2, "_0.05padj_1.5FC.txt", sep=""))
             ,sep="\t", quote=FALSE)
-
-write.table(res,
-            file.path(output_dir,paste(compare1,"v",compare2,"_","clone_stage","_all.txt", sep=""))
-            ,sep="\t", quote=FALSE)
-
-tbl <- res[ which(res$padj < 0.05 & abs(res$log2FoldChange) > 0.5849625007211562),]
-sigDE <- rownames(tbl) 
-write.table(sigDE, file.path(output_dir, "AroMoose&LK16&PA42&POVI4&SW4.txt"), sep="\t", quote=FALSE, 
-            row.names = FALSE, col.names = FALSE)
-
-
-library("pcaExplorer")
-pcaExplorer(dds = dds)
